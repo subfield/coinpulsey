2a4251a5053edbdbff40b036ca9e59eb696791d7
subfield <tetelainv@gmail.com>
2026-01-19 02:54:58 +0100

    feat: Integrate shadcn/ui components, custom theming, and update Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typedRoutes: true,
+  experimental: {
+    typedEnv: true,
+  }
 };
 
 export default nextConfig;
